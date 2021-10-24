71f345e2a96c048db6179dae4e81f9c853e376be
Alexandra Atzl <aatzl@bn.co>
2021-10-24 18:27:11 -0400

    6. Get test to pass.

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 export function checkForRestrictedWords(inputStr)  {
   if (inputStr.indexOf('wtf') >= 0) {
     return true;
+  }  else if (inputStr.indexOf('@$$') >= 0) {
+    return  true;
   }
   return false;
 }
